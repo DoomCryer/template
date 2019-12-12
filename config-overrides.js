9639e5085465c1a42af99e29d5b200b168c65f89
Evgeniy Smirnov <esmirnov@jtconsulting.ru>
2019-12-12 17:47:10 +0300

    init

---
+++ config-overrides.js
@@ -0,0 +1,16 @@
+const { override, fixBabelImports, addLessLoader } = require("customize-cra");
+
+module.exports = override(
+  fixBabelImports("import", {
+    libraryName: "antd",
+    libraryDirectory: "es",
+    style: true
+  }),
+  addLessLoader({
+    javascriptEnabled: true,
+    modifyVars: {
+      // To modify global colors of antd components
+      // "@primary-color": "#1DA57A" 
+    }
+  })
+);
